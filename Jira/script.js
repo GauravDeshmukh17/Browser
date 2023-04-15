8888dcf205b99228ff8ea7d96bf0197136df868f
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2023-04-15 14:43:03 +0530

    :zap: ticket created

--- Jira/script.js
+++ Jira/script.js
@@ -1,6 +1,10 @@
 let addbtn=document.querySelector(".add-btn");
 let modalCont=document.querySelector(".modal-cont");
 
+let modalPriorityColor='black';
+let textAreaCont=document.querySelector('.textAreaCont');
+let mainCont=document.querySelector('.main-cont');
+
 let isModalPresent=false;
 addbtn.addEventListener('click',function(){
     if(!isModalPresent){
@@ -32,7 +36,34 @@ allPriorityColors.forEach(function(colorElement){
             priorityColorElement.classList.remove('active');
         })
         colorElement.classList.add('active');
+        modalPriorityColor=colorElement.classList[0]; 
     })
 })
+
+modalCont.addEventListener('keydown',function(e){
+    // console.log(e);
+    let key=e.key;
+    if(key=='Shift'){
+        // console.log(modalPriorityColor);
+        // console.log(textAreaCont.value);
+        createTicket(modalPriorityColor,textAreaCont.value);
+        modalCont.style.display="none";
+        isModalPresent=false;
+    }
+})
+
+function createTicket(ticketColor,data){
+    divElement=document.createElement("div");
+    divElement.setAttribute('class','ticket-cont');
+    divElement.innerHTML=`
+        <div class="ticket-color ${ticketColor}"></div>
+        <div class="ticket-id"></div>
+        <div class="text-area">${data}</div>
+    `;
+
+    mainCont.appendChild(divElement);
+}
+
+
 
 
